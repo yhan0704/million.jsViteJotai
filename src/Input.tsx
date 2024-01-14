13d66d5df264afd71bd076d7aa4d56ab9ebdfaad
yhan0704 <hanyc1205@gmail.com>
2024-01-14 15:30:28 -0500

    split component and used useAtomValue/useAtom

--- src/Input.tsx
+++ src/Input.tsx
@@ -1,16 +1,19 @@
 // Input.tsx
 import React, { ChangeEvent } from 'react';
-import { atom, useAtom } from 'jotai';
+import { atom, useAtom, useAtomValue } from 'jotai';
+import { countAtom } from './Uppercase';
 
 export const textAtom = atom('hello')
 
 const Input: React.FC = () => {
     const [text, setText] = useAtom(textAtom)
+    const count = useAtomValue(countAtom)
+
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => setText(e.target.value)
     return (
         <>
             <input value={text} onChange={handleChange} />
-            <div>{text}</div>
+            <p>{count}</p>
         </>
     )
 };
--- src/Uppercase.tsx
+++ src/Uppercase.tsx
@@ -1,21 +1,22 @@
 // Uppercase.tsx
 import React from 'react';
-import { atom, useAtom } from 'jotai';
-
-export const textAtom = atom('hello')
-const uppercaseAtom1 = atom(
-    (get) => get(textAtom).toUpperCase()
-);
+import { atom, useAtom, useAtomValue, useSetAtom } from 'jotai';
+import { textAtom } from './Input';
 
 const uppercaseAtom = atom(
     (get) => get(textAtom).toUpperCase()
 )
+export const countAtom = atom(0)
 
 const Uppercase: React.FC = () => {
-    const [uppercase] = useAtom(uppercaseAtom)
-    console.log(uppercase)
+    const setCount = useSetAtom(countAtom)
+    const count = useAtomValue(countAtom)
+
     return (
-        <div>Uppercase: {uppercase}</div>
+        <>
+            <div>count: {count}</div>
+            <button onClick={() => setCount(count + 1)}>+1</button>
+        </>
     )
 };
 
