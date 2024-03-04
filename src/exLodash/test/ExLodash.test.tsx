05ba5a448620e6f03084b051a001f55ffb5ac498
yhan0704 <hanyc1205@gmail.com>
2024-03-04 09:04:21 -0500

    can use tobeindocument

--- src/exLodash/test/ExLodash.test.tsx
+++ src/exLodash/test/ExLodash.test.tsx
@@ -1,23 +1,28 @@
 import { render, waitFor } from "@testing-library/react";
 import ExLodash from "../ExLodash";
 import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
 
 jest.useFakeTimers();
 
 describe("Counter Component", () => {
   it("should show filtered options after 0.5 seconds of typing", async () => {
-    const { getByPlaceholderText, getByText } = render(<ExLodash />);
+    const { getByPlaceholderText, queryByText } = render(<ExLodash />);
     const input = getByPlaceholderText("Favorites");
 
     // Type 'The' into the input
-    userEvent.type(input, "hi");
+    userEvent.type(input, "bye");
 
     jest.advanceTimersByTime(500);
 
     // Wait for the debounce to complete
     await waitFor(() => {
-      expect(getByText("The-Godfather-Redemption-hi-mango")).toBeTruthy();
-      expect(getByText("The-Godfather-Redemption-hi-mango")).toBeTruthy();
+      expect(
+        queryByText("The-Godfather-Redemption-hi-banana")
+      ).not.toBeInTheDocument();
+      expect(
+        queryByText("The-Godfather-Redemption-hi-pineapple")
+      ).not.toBeInTheDocument();
     });
   });
 });
